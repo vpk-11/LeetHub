40679af51ca50127e61296849e290d9787f0688d
vpk-11 <83803726+vpk-11@users.noreply.github.com>
2026-08-21 19:20:43 +0400

    Strip dead OAuth message handling from background worker

--- scripts/background.js
+++ scripts/background.js
@@ -12,33 +12,7 @@ api.runtime.onInstalled.addListener(details => {
 api.runtime.onMessage.addListener(handleMessage);
 
 function handleMessage(request, sender, sendResponse) {
-  if (request && request.closeWebPage === true && request.isSuccess === true) {
-    /* Set username */
-    api.storage.local.set({ leethub_username: request.username });
-
-    /* Set token */
-    api.storage.local.set({ leethub_token: request.token });
-
-    /* Close pipe */
-    api.storage.local.set({ pipe_leethub: false }, () => {
-      console.log('Closed pipe.');
-    });
-
-    api.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-      var tab = tabs[0];
-      api.tabs.remove(tab.id);
-    });
-
-    /* Go to onboarding for UX */
-    const urlOnboarding = api.runtime.getURL('welcome.html');
-    api.tabs.create({ url: urlOnboarding, active: true }); // creates new tab
-  } else if (request && request.closeWebPage === true && request.isSuccess === false) {
-    alert('Something went wrong while trying to authenticate your profile!');
-    api.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-      var tab = tabs[0];
-      api.tabs.remove(tab.id);
-    });
-  } else if (request.type === 'LEETCODE_SUBMISSION') {
+  if (request.type === 'LEETCODE_SUBMISSION') {
     api.webNavigation.onHistoryStateUpdated.addListener(
       (e = function (details) {
         const submissionId = details.url.match(/\/submissions\/(\d+)\//)[1];
